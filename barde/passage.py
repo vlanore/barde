776a6ef6a472dc6f0396871b3dfac5b7f11a57f4
Vincent Lanore <vincent.lanore@gmail.com>
2022-02-22 17:46:52 +0000

    Minor import fix

--- barde/passage.py
+++ barde/passage.py
@@ -1,10 +1,7 @@
 import sys
 from typing import Callable, Optional
 
-from browser import (  # type:ignore ; pylint: disable=import-error
-    document,
-    html as bh,
-)
+from browser import document  # type:ignore ; pylint: disable=import-error
 from browser.local_storage import storage  # type:ignore ; pylint: disable=import-error
 from browser.object_storage import (  # type:ignore ; pylint: disable=import-error
     ObjectStorage,
